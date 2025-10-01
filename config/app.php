376ff9aa36f9243cd3dbcf3987a72792bfa1d6a9
RAugadh <ankit.kumar060@gmail.com>
2025-10-01 18:33:24 +0530

    #added livewire custom update route

--- config/app.php
+++ config/app.php
@@ -56,6 +56,8 @@ return [
 
     'url' => env('APP_URL', 'http://localhost'),
 
+    'asset_prefix' => env('ASSET_PREFIX', ''),
+
     /*
     |--------------------------------------------------------------------------
     | Application Timezone
